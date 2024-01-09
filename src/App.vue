407cce62c2829d7c8dbb6567fb800a54ec7e040b
BenfanteMarco <145206234+BenfanteMarco@users.noreply.github.com>
2024-01-09 03:02:39 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+export default {
+  components: {
+    AppHeader
+  }
+}
+</script>
+<template lang="">
+  <div>
+    <AppHeader />
+  </div>
+</template>
+<style lang="scss">
+  @use './styles/partials/variables' as *;
+  @use './styles/generals.scss';
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,83 @@
+<script>
+export default {
+    name: 'AppHeader',
+    data() {
+        return{
+            menu: [
+                {
+                    label: "Characters",
+                    active: false
+                },
+                {
+                    label: "Comics",
+                    active: true
+                },
+                {
+                    label: "Movies",
+                    active: false
+                },
+                {
+                    label: "TV",
+                    active: false
+                },
+                {
+                    label: "Games",
+                    active: false
+                },
+                {
+                    label: "Collectibles",
+                    active: false
+                },
+                {
+                    label: "Videos",
+                    active: false
+                },
+                {
+                    label: "Fans",
+                    active: false
+                },
+                {
+                    label: "News",
+                    active: false
+                },
+                {
+                    label: "Shop",
+                    active: false
+                },
+            ]
+        }
+    }
+}
+</script>
+<template lang="">
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="d-flex justify-content-between align-items-center mt-3">
+                        <img src="../assets/img/dc-logo.png" alt="logo">
+                        <ul class="d-flex gap-3 list-unstyled text-uppercase">
+                            <li v-for="nav, index in menu" :key="index">
+                                <a href="#" :class="nav.active ? 'active' : ''">{{nav.label}}</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="scss" scoped>
+@use '../styles/generals.scss';
+
+a {
+    text-decoration: none;
+    color: black;
+    font-weight: 300;
+}
+
+.active {
+    color: #0083F8;
+    border-bottom: 3px solid #0083F8;
+}
+</style>
